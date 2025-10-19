d4b288217be59399c76cad9f94fed390d7b9d6a0
tramplin30 <vicodin28@gmail.com>
2025-10-19 14:20:25 +0300

    demoqa: radio buttons test done

--- tests/homeWork/demoqaSite_tests/radio-buttonCheck.spec.ts
+++ tests/homeWork/demoqaSite_tests/radio-buttonCheck.spec.ts
@@ -1,6 +1,6 @@
 //використовуючи ЛИШЕ Xpath 
 
-import { test, expect } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
 import * as Locators from './xlocators';
 
     test.beforeEach ('Open Site', async ({ page }) => {
@@ -10,13 +10,59 @@ import * as Locators from './xlocators';
     })
 
 
+    const xYesRadioButton: string = "//input[@id='yesRadio']";
+    const xImpressiveRadioButton: string = "//input[@id='impressiveRadio']";
+    const xNoRadioButton: string = "//input[@id='noRadio']";
+
+
+    async function isRadioAvailable(page: Page, xPathRadio: string): Promise<string> {
+        
+        const radio: Locator = page.locator(xPathRadio);
+        const status: boolean = await radio.isDisabled();
+
+        if (status == true) {
+            console.log("disabled");
+            return "disabled";
+
+        } else {
+            console.log("enabled");
+            return  "enabled";}
+            
+        
+    } 
+
+
+    async function getRadioState(page: Page, xPathRadio: string): Promise<string> {
+        
+        const status: boolean = await page.locator(xPathRadio).isChecked();
+
+        if (status == true) {
+            console.log("checked");
+            return "checked";
+
+        } else {
+            console.log("unchecked");
+            return  "unchecked";}
+            
+        
+    } 
+
+
     test('ID   Some: text', 
     {
         tag: ["@tag"],
         annotation: {type: "description", description: "description"}
     },
     async ({ page }) => {
-        
 
+        var radioList: string[] = [xYesRadioButton, xImpressiveRadioButton, xNoRadioButton]
+
+        for (var radio of radioList ) {
+            if (await isRadioAvailable(page, radio) == "enabled" && await getRadioState(page, radio) == "unchecked") {
+
+            await page.locator(radio + "/following-sibling::label").click();
+            expect(await getRadioState(page, radio)).toBe("checked")
+
+        } else console.log("your radioButton: " + "\'" + (await page.locator(radio + "/following-sibling::label").allTextContents()) + "\'" + " - is not available or already checked !")}
         
     })
