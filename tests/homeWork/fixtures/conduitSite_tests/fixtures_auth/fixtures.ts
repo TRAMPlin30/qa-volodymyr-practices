7b76e10f8dc51f6e8c86ffb256db09e707547cdf
tramplin30 <vicodin28@gmail.com>
2025-12-07 17:17:00 +0200

    fixtures

--- tests/homeWork/fixtures/conduitSite_tests/fixtures_auth/fixtures.ts
+++ tests/homeWork/fixtures/conduitSite_tests/fixtures_auth/fixtures.ts
@@ -7,21 +7,35 @@ import { SignUpPage } from './SignUpPage'
 // const my_test = test.extend({ объект })  //расширяеться объектами
 
 
-type MyFixtures  = { signInPage: SignInPage, signUpPage: SignUpPage } 
+type MyFixtures  = { signInPage: SignInPage, signUpPage: SignUpPage, beforeTextures: void } 
 
 
 export const my_test = test.extend<MyFixtures>({
-    
-    //фикстура одной страници - обычная асинх ыункция которая возвращает объект страницы что б использовать ешо в тестах потом
+    //lazy loading - при таком синтаксисе будет загружать только при вызове
+    //фикстура одной страници - обычная асинх стрелочная функция которая возвращает объект страницы что б использовать ешо в тестах потом
     signInPage: async ({page}, use) => {               
         const signInPage = new SignInPage(page);
         await use(signInPage);
+        console.log("fixture 1")
     },
 
-    //фикстура другой страници - обычная асинх ыункция которая возвращает объект страницы что б использовать ешо в тестах потом
+    //lazy loading - при таком синтаксисе будет загружать только при вызове
+    //фикстура другой страници - обычная асинх стрелочная функция которая возвращает объект страницы что б использовать ешо в тестах потом
     signUpPage: async ({page}, use) => {
         const signUpPage = new SignUpPage(page);
         await use(signUpPage);
-    }
+        console.log("fixture 2")
+    },
+
+
+    //притаком синтаксисе фикстура будет выполнена автоматически
+    beforeTextures: [async({signInPage}, use) => { //передаем предидущую фикстуру что б был объект signInPage
+        await signInPage.goto();
+
+    }, {auto: true}]
 
 })
+
+
+
+
--- tests/homeWork/fixtures/conduitSite_tests/fixtures_auth/signIn.spec.ts
+++ tests/homeWork/fixtures/conduitSite_tests/fixtures_auth/signIn.spec.ts
@@ -6,7 +6,9 @@ import { expect } from '@playwright/test';
   
     my_test('ID   Some: text', {tag: ["@tag"], annotation: {type: "description", description: "description"}},
     
-    async ({ signInPage, signUpPage }) => {   //фикстуры из файла fixtures.ts
+    async ({ signInPage, signUpPage }) => {   //фикстуры из файла fixtures.ts (там может быть что угодно: создание объектов, вывод чего-то в консоль, каието методы что угодно)
+                                              // и срабатывает это все внутри - достаточно для вызова только передать их как аргументы в async({fix1, fix2, ...})
+                                              // все что указано в use() там в фикстуре будет использовано и вызвано !!!
         
 
 
